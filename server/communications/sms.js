6fd043d017c7acc96eb3af42038e6d43aa168507
BFriedman117 <binyaminf@yahoo.com>
2018-05-01 01:11:09 -0400

    I give up

--- server/communications/sms.js
+++ server/communications/sms.js
@@ -1,7 +1,5 @@
 const Twilio = require('twilio')
-// const { ACCOUNT_SID, AUTH_TOKEN } = require('../../secrets')
+const { ACCOUNT_SID, AUTH_TOKEN } = require('../../secrets')
 
-// const Client = new Twilio(ACCOUNT_SID || process.env.ACCOUNT_SID, AUTH_TOKEN || process.env.AUTH_TOKEN)
-const Client = new Twilio('AC2358da5c2ac2a005f53aee0846f76b85', '82480838e9b41ca2fc04c81978fe2b6a')
-
+const Client = new Twilio(ACCOUNT_SID, AUTH_TOKEN)
 module.exports = { Client }
