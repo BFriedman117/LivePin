188cc7bdbb159f223ba007b1a23bb2fb17ac9e50
BFriedman117 <binyaminf@yahoo.com>
2018-05-01 00:58:21 -0400

    one more try to fix heroku probs

--- server/communications/sms.js
+++ server/communications/sms.js
@@ -1,6 +1,6 @@
 const Twilio = require('twilio')
 const { ACCOUNT_SID, AUTH_TOKEN } = require('../../secrets')
 
-const Client = new Twilio(ACCOUNT_SID, AUTH_TOKEN)
+const Client = new Twilio(ACCOUNT_SID  || process.env.ACCOUNT_SID, AUTH_TOKEN || process.env.ACCESS_TOKEN)
 
 module.exports = { Client }
